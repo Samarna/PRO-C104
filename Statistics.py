a9e0ce1bc41f128cdd10b535ee078526f4df2526
Samarna <63645732+Samarna@users.noreply.github.com>
2021-04-24 18:36:40 +0530

    Update Statistics.py

--- Statistics.py
+++ Statistics.py
@@ -9,8 +9,8 @@ with open ('HeightWeight.csv',newline = '') as f:
 
     new_data = []
     for i in range(len(file_data)):
-        height = file_data[i][1]
-        new_data.append(float(height))
+        weight = file_data[i][2]
+        new_data.append(float(weight))
 
     # Calculating Mean
     sum = 0
@@ -32,26 +32,5 @@ with open ('HeightWeight.csv',newline = '') as f:
         median = new_data[n//2]
     print(str(median))
     print(n)
-
-    #Calculating Mode
-    data = collections.Counter(new_data)
-    mode_range = {"50-60":0,"60-70":0,"70-80":0}
-    for height,occurance in data.items():
-        if 50<float(height)<60:
-            mode_range["50-60"]+=occurance
-
-        elif 60<float(height)<70:
-            mode_range["60-70"]+=occurance
-        
-        elif 70<float(height)<80:
-            mode_range["70-80"]+=occurance
-    #print(mode_range)
-
-    m_range,m_occurance = 0,0
-    for range,occurance in mode_range.items():
-        if occurance>m_occurance:
-            m_range,m_occurance = [int(range.split("-")[0]),int(range.split("-")[1])],occurance
-    mode = float((m_range[0]+m_range[1])/2)
-    print(mode)
-
+    
     print(str(statistics.mode(new_data)))
